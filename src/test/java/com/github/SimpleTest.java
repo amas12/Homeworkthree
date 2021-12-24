21dbc9812b5a4e2110e111f5a4296ca0d73d081c
Albina <Maestro1987>
2021-12-24 15:40:51 +0300

    corrected 1

--- src/test/java/com/github/SimpleTest.java
+++ src/test/java/com/github/SimpleTest.java
@@ -8,10 +8,17 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleTest {
-        /*$("h1 div")
-        он найдет первый div внутри h1 любого уровня
-        $("h1").$("div")
-        он найдет первый div внутри h1 первого уровня*/
+    /*$("h1 div")
+    он найдет первый div внутри h1 любого уровня
+    $("h1").$("div")
+    он найдет первый div внутри h1 первого уровня*/
+    @Test
+    void h1DivVsH1Div() {
+        open("https://onlinecontract.ru/");
+        $(".div h2");
+        $(".div").$("h1");
+    }
+
 
     @Test
     void BrowserCommandExamples() {
@@ -21,9 +28,10 @@ public class SimpleTest {
         $$("ul.repo-list li").first().$("a").click(); //кликнуть на первый элемент
         $("h1").shouldHave(text("selenide / selenide"));//проверить текст selenide
         $("#wiki-tab").click();//Перейдите в раздел Wiki проекта
-        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
-        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-        //$(".Layout-main").$(byText("Soft assertions")).click();//Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        //$("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        sleep(4000);
+        //$("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".Layout-main").$(byText("Soft assertions")).click();//Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         //$$("ol[start=\"3\"]").find(visible).scrollTo().shouldHave(text("Using JUnit5 extend test class:"));//Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
@@ -35,7 +43,6 @@ public class SimpleTest {
         $("#column-a").dragAndDropTo("#column-b");//Перенесите прямоугольник А на место В
         $("#column-a").shouldHave(text("B"));//Проверьте, что прямоугольники действительно поменялись
         $("#column-b").shouldHave(text("A"));
-        sleep(5000);
     }
 
 }
